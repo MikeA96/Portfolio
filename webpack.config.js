ce1a69c0e0ebbef6ecfeabe5cf27316e4965f207
Mike <mink@Mikes-MBP.lan>
2021-05-11 13:03:08 -0400

    deploying to heroku'

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = function(_env, argv) {
    output: {
      path: path.resolve(__dirname, "dist"),
       filename: "assets/js/[name].[contenthash:8].js",
-      publicPath:"/"
+      publicPath:"https://mike-allans-portfolio.herokuapp.com/"
     },
     devServer:{
         contentBase:path.join(__dirname,'./dist/assets/')
