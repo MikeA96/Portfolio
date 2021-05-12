b46e7a2dfdd5075413712cd6415fe06749b80fa2
Mike <mink@Mikes-MBP.lan>
2021-05-12 14:16:39 -0400

    deploying to heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const path= require('path')
+app.use(cors());
+
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, "/dist")));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + 'dist/index.html'));
+});
+
+app.listen(PORT, () => console.log("Listening on port 8080"));
